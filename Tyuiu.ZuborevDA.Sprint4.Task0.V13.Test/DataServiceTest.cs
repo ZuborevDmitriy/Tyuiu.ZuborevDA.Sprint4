46b5b503a319d66a6cad3fb9bce8f725ef15fcd9
Dmitriy Zuborev <Tyuiu.ZuborevDA@outlook.com>
2023-11-02 22:23:35 +0500

    Task1

--- Tyuiu.ZuborevDA.Sprint4.Task0.V13.Test/DataServiceTest.cs
+++ Tyuiu.ZuborevDA.Sprint4.Task0.V13.Test/DataServiceTest.cs
@@ -15,7 +15,7 @@ namespace Tyuiu.ZuborevDA.Sprint4.Task0.V13.Test
 
             int[] numsArray = { 2, 6, 2, 3, 4, 5, 4, 9, 7, 8 };
             int res = ds.GetSumEvenArrEl(numsArray);
-            int wait = 18;
+            int wait = 26;
             Assert.AreEqual(wait, res);
         }
     }
