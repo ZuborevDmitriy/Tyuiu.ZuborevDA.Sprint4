ef456d0a1ae9922a658a5ab203d46909ce9ec71d
Dmitriy Zuborev <Tyuiu.ZuborevDA@outlook.com>
2023-11-03 00:22:50 +0500

    Task3

---
+++ Tyuiu.ZuborevDA.Sprint4.Task3.V9.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ZuborevDA.Sprint4.Task3.V9.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ZuborevDA.Sprint4.Task3.V9.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a1756106-5578-4215-9125-a0c818ac5901")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
